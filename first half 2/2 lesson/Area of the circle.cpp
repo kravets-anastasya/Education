47e72d95d3943b79cce614ac2fad7ba2804c0a76
kravets-anastasya <97699106+kravets-anastasya@users.noreply.github.com>
2023-01-13 18:06:18 +0300

    Add files via upload

---
+++ first half 2/2 lesson/Area of the circle.cpp
@@ -0,0 +1,27 @@
+﻿/*По заданной длине окружности найти площадь круга
+по формуле S = pi*R2, радиус вычислить из формулы длины
+окружности: L=2*pi *R.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float S = 0;
+	float pi = 3.14;
+	float R = 0;
+	float L = 0;
+
+	cout << "pi = " << pi << endl;
+	cout << "Circumference_length = ";
+	cin >> L;
+
+	R = L / (2 * pi);
+	//L = 2 * pi * R;
+	cout << "Radius = " << R << endl;
+
+	S = pi * (R*R);
+	cout << "Area_of_the_circle = " << S << endl;
+
+	return 0;
+}
---
+++ first half 2/2 lesson/Resistance value.cpp
@@ -0,0 +1,23 @@
+﻿//Заданы три сопротивлении R1, R2, R3. Вычислить значение сопротивления R0 по формуле: 1/R0 = 1/R1+1/R2+1/R3.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float R1 = 0;
+	float R2 = 0;
+	float R3 = 0;
+	float R0 = 0;
+
+	cout << "R1 = ";
+	cin >> R1;
+	cout << "R2 = ";
+	cin >> R2;
+	cout << "R3 = ";
+	cin >> R3;
+
+	R0 = 1 / R1 + 1 / R2 + 1 / R3;
+	cout << "R0 = " << 1/R0 << endl; 
+
+	return 0;
+}
