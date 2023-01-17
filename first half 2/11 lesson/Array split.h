ef31584e978236f66c8d5518a77ace704dd3fc93
kravets-anastasya <97699106+kravets-anastasya@users.noreply.github.com>
2023-01-17 21:27:00 +0300

    Add files via upload

---
+++ first half 2/11 lesson/Array split.h
@@ -0,0 +1,31 @@
+#pragma once
+//2. Сделать заголовочный файл с функциями по разделению массива на 2 части.
+#include <iostream>
+#include <clocale>
+#include <stdlib.h>
+
+using namespace std;
+
+void Conclusion(int array[], int size) {
+	cout << "{";
+	for (size_t i = 0; i < size; i++) {
+		cout << array[i] << ", ";
+	}
+	cout << "}" << endl << endl;
+}
+void ManualInputToArray(int array[], int size) {
+	for (size_t i = 0; i < size; i++) {
+		cout << "Введите ваш доход за " << i + 1 << " месяц: ";
+		cin >> array[i];
+	}
+}
+void ArraySplit_1_Half(int array[], int first_half[], int size_2) {
+	for (size_t i = 0; i < size_2; i++) {
+		first_half[i] = array[i];
+	}
+}
+void ArraySplit_2_Half(int array[], int second_half[], int size_2) {
+	for (size_t i = 0; i < size_2; i++) {
+		second_half[i] = array[i + size_2];
+	}
+}
