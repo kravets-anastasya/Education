288c906ce296743c985a86c434caab6130786fc5
kravets-anastasya <97699106+kravets-anastasya@users.noreply.github.com>
2023-02-06 16:45:47 +0300

    Add files via upload

---
+++ first half 2/12 lesson/Dynamic array functions.cpp
@@ -0,0 +1,70 @@
+﻿// Кравец Анастасия Сергеевна
+/*Написать следующие функции для работы
+с динамическим массивом:
+■ Функция добавления элемента в конец массива.
+■ Функция вставки элемента по указанному индексу.
+■ Функция удаления элемента по указанному индексу.*/
+
+#include <iostream>
+#include "Working with array creation.h"
+#include "Functions in an array.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int size{};
+	int additional_number{};
+	int index{};
+	int menu_selection{};
+	cout << "Введите, количество элементов \n";
+	cin >> size;
+	int* array = new int[size];
+
+	cout << "Выберете: 1- ввенсти вручную, 2- рандомные числа.\n";
+	cin >> menu_selection;
+	if(menu_selection == 2){
+		NumberGenerator(array, size);
+	}
+	else if (menu_selection == 1) {
+		ManualInputToArray(array, size);
+	}
+	else {
+		cout << "Вы ввели неверное число!";
+		return -1;
+	}
+
+	Conclusion(array, size);
+
+	cout << "\nВыберете, что вы хотите сделать с массивом:\n";
+	cout << "1. Добавления элемента в конец массива.\n";
+	cout << "2. Вставки элемента по указанному индексу.\n";
+	cout << "3. Удаления элемента по указанному индексу.\n\n";
+	cin >> menu_selection;
+
+	if (menu_selection == 1) {
+	//■ Функция добавления элемента в конец массива.
+	cout << "Введите значение, которое хотите внести в конец массива\n";
+	cin >> additional_number;
+	int* new_arr = Element_to_end(array, size, additional_number);
+	Conclusion(new_arr, size + 1);
+
+	}else if(menu_selection == 2){
+	//■ Функция вставки элемента по указанному индексу.
+	cout << "Введите индекс, по которому хотите внести элемент в массив\n";
+	cin >> index;
+	cout << "Введите значение, которое хотите внести в массив по данному элементу\n";
+	cin >> additional_number;
+	int* new_arr = Insert_by_index(array, size, index, additional_number);
+	Conclusion(new_arr, size + 1);
+
+	}else if(menu_selection == 3){
+	//■ Функция удаления элемента по указанному индексу.
+	cout << "Введите индекс, по которому хотите удалить элемент в массив\n";
+	cin >> index;
+	int* new_arr = Delete_by_index(array, &size, index);
+	Conclusion(new_arr, size);
+	}
+
+	return 0;
+}
---
+++ first half 2/12 lesson/Functions in an array.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+//■ Функция добавления элемента в конец массива.
+int* Element_to_end(int array[], int size, int additional_number) {
+	int* new_arr = new int[size + 1];
+
+	for (size_t i = 0; i < size + 1; i++){
+		new_arr[i] = array[i];
+	}
+	new_arr[size] = additional_number;
+	return new_arr;
+}
+
+//■ Функция вставки элемента по указанному индексу.
+int* Insert_by_index(int array[], int size, int index, int additional_number) {
+	int* new_arr = new int[size + 1];
+	for (size_t i = 0, j = 0; i <= size; i++) {
+		if (j == index) {
+			new_arr[j] = additional_number;
+			i--;
+		}
+		else {
+			new_arr[j] = array[i];
+		}
+		j++;
+	}
+	return new_arr;
+}
+
+//■ Функция удаления элемента по указанному индексу.
+int* Delete_by_index(int array[], int* size, int index) {
+	int new_size = *size - 1;
+	int* new_arr = new int[new_size];
+	for (size_t i = 0, j = 0; i < *size; i++) {
+		if (i == index) {
+			continue;
+		}
+		else {
+			new_arr[j] = array[i];
+		}
+		j++;
+	}
+	*size = new_size;
+	return new_arr;
+}
